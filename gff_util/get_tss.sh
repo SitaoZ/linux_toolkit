5a97d39ec2fadf00c4a9f6d324bdf60d1d1508ec
zhusitao <zhusitao1990@163.com>
2022-10-27 14:31:14 +0800

    extract tss bed and promoter

--- gff_util/get_tss.sh
+++ gff_util/get_tss.sh
@@ -1,1 +1,5 @@
-cat xxx.gff3 | awk '$3=="gene"{print $0}' | grep protein_coding | awk -v OFS="\t" '{if ($7=="+") {print $1, $4, $4+1}} else {print $1, $5-1, $5}' > tss.bed
+# tss
+cat xxx.gff3 | awk '$3=="gene" {print $0}' | grep protein_coding | awk -v OFS="\t" '{if ($7=="+") {print $1, $4, $4+1}} else {print $1, $5-1, $5}' > tss.bed
+
+# promoter 5k upstream from tss
+cat xxx.gff3 | awk '$3=="gene" {print $0}' | grep protein_coding | awk -v OFS="\t" '{if ($7=="+") {print $1, $4, $4+5000} else {print $1, $5-5000, $5}}' > promoters.bed
