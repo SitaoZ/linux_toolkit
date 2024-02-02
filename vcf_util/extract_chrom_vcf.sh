67910ad36e0569107cd97204b9f09b32d86c88af
Sitao Zhu <zhusitao1990@163.com>
2024-02-02 10:30:32 +0800

    Delete vcf_util directory

--- vcf_util/extract_chrom_vcf.sh
+++
@@ -1,9 +0,0 @@
-for chr in {1..22}; do awk -v chr=$chr -v threshold=10 '$1 == chr && $6 > threshold' xxx.vcf > xxx_chr$chr.vcf; done
-# https://samtools.github.io/hts-specs/VCFv4.2.pdf
-# shell中的变量在awk中无法识别，可以使用-v指定变量传入awk
-# -v chr=$chr, 将shell中的变量$chr传入awk
-
-
-awk '$1 == 5 && $7 == "PASS"' xxx.vcf > xxx_filter.vcf
-# 挑选五号染色体上的SNP
-# 过滤值为PASS
