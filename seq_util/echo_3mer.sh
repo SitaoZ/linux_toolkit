3960c290e682c19522bba01e5109db5c2f9f644e
Sitao Zhu <zhusitao1990@163.com>
2024-02-02 10:36:06 +0800

    Delete seq_util directory

--- seq_util/echo_3mer.sh
+++
@@ -1,2 +0,0 @@
-# 打印所有3mer的DNA组合
-echo {A,C,T,G}{A,C,T,G}{A,C,T,G}
--- seq_util/reverse_complement.sh
+++
@@ -1,3 +0,0 @@
-echo 'AGTCATGCAGTGCNNNNT' | rev | tr 'ACTG' 'TGAC'
-
-echo 'AGTCATGCAGTGCNNNNT' | python -c "import sys;from Bio.Seq import Seq;a = [print(Seq(i.strip()).reverse_complement()) for i in sys.stdin];"
